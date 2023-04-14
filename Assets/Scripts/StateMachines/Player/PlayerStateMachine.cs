c2f986a76ac02f6d0d70dc9e23af38c5bab6420d
alaskaThunderfx <51172480+alaskaThunderfx@users.noreply.github.com>
2023-04-14 01:05:31 -0400

    Add camera relative movement
    Adds logic so that the character moves in relation to the direction the camera
    is facing vs directions based on the world.

--- Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
+++ Assets/Scripts/StateMachines/Player/PlayerStateMachine.cs
@@ -8,14 +8,22 @@ namespace StateMachines.Player
         // Public variables for Character prefab components
         [field: SerializeField] public InputReader InputReader { get; private set; }
         [field: SerializeField] public CharacterController CharacterController { get; private set; }
+
         [field: SerializeField] public Animator Animator { get; private set; }
-        
+
+        // Public variable for the Camera that will be set in code
+        public Transform MainCameraTransform { get; private set; }
+
         // Public variables representing values
         [field: SerializeField] public float FreeLookMovementSpeed { get; private set; }
 
         // Built-in methods
         void Start()
         {
+            // Confirms that the main camera exists, then sets the MainCameraTransform value to be equal to the
+            // transform of the main camera in Unity
+            if (Camera.main != null) MainCameraTransform = Camera.main.transform;
+            
             // Switches state when PlayerStateMachine is called
             SwitchState(new PlayerTestState(this));
         }
--- Assets/Scripts/StateMachines/Player/PlayerTestState.cs
+++ Assets/Scripts/StateMachines/Player/PlayerTestState.cs
@@ -18,14 +18,7 @@ namespace StateMachines.Player
         public override void Tick(float deltaTime)
         {
             // Establish the movement variable for use with transform.Translate
-            Vector3 movement = new Vector3();
-
-            // Set the X value of movement to the X value being read by the InputReader
-            movement.x = StateMachine.InputReader.MovementValue.x;
-            // Set the Y value of movement to 0, as we want the player to only be able to move on the ground
-            movement.y = 0;
-            // Set the Z value of movement to the Y value being read by the InputReader
-            movement.z = StateMachine.InputReader.MovementValue.y;
+            Vector3 movement = CalculateMovement();
 
             // This uses the CharacterControllers built in Move method. It will move the character in the direction
             // based on the values it gets from the movement variable, the speed of which is dictated by the
@@ -35,14 +28,14 @@ namespace StateMachines.Player
 
             // Checks to see if any values representing movement are being sent by the InputReader
             if (StateMachine.InputReader.MovementValue == Vector2.zero)
-            {   
+            {
                 // If it is not, the FreeLookSpeed variable in the Animator will be set to zero, which indicates that 
                 // the idle animation should be playing
                 StateMachine.Animator.SetFloat("FreeLookSpeed", 0, 0.1f, deltaTime);
                 // Breaks the Tick loop here
                 return;
             }
-            
+
             // If there are movement values being sent from the InputReader, it changes the value of the FreeLookSpeed
             // Animator variable to 1, which indicates that the running animation should be playing
             StateMachine.Animator.SetFloat("FreeLookSpeed", 1, 0.1f, deltaTime);
@@ -54,6 +47,29 @@ namespace StateMachines.Player
 
         public override void Exit()
         {
+        }
+
+        // Private methods
+
+        // Method that handles calculating the movement based on factors within the body of this method.
+        private Vector3 CalculateMovement()
+        {
+            // Finds the forward and right direction in relation to the main camera
+            Vector3 forward = StateMachine.MainCameraTransform.forward;
+            Vector3 right = StateMachine.MainCameraTransform.right;
+
+            // Sets their Y values to 0 as we only care about where forward is and whether it is looking left/right
+            forward.y = 0;
+            right.y = 0;
+
+            // Sets the magnitude of each of these to 1
+            forward.Normalize();
+            right.Normalize();
+
+            // Returns the adjusted movement values now in relation to where the camera is facing vs. just in the world
+            // space
+            return forward * StateMachine.InputReader.MovementValue.y +
+                   right * StateMachine.InputReader.MovementValue.x;
         }
     }
 }
